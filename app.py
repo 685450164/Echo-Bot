e2543904c8d170d7fff304208dba399aaf3f7c86
685450164 <685450164@alumni.tku.edu.tw>
2025-01-02 14:31:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,59 +1,56 @@
 from flask import Flask, request, abort
-
 from linebot.v3 import (
     WebhookHandler
 )
 from linebot.v3.exceptions import (
     InvalidSignatureError
 )
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
+    PostbackEvent
+)
 from linebot.v3.messaging import (
     Configuration,
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
+    TextMessage,
+    QuickReply,
+    QuickReplyItem,
+    PostbackAction
 )
 
 import os
 import dotenv
 
+#Azure Translation
 from azure.ai.translation.text import TextTranslationClient
 from azure.core.credentials import AzureKeyCredential
 from azure.core.exceptions import HttpResponseError
 
-
-dotenv.load_dotenv()
-
-# print('結果:' + os.getenv('CHANNEL_ACCESS_TOKEN'))
-
 app = Flask(__name__)
 
-configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
-line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
+handler = WebhookHandler(CHANNEL_SECRET)
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # parse webhook body
     try:
-        line_handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
-
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_messsage(event):
